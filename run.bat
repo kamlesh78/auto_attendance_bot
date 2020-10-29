d979435c6de180ef0263181c7367d0998496463f
kamlesh singh <30825138+kamlesh78@users.noreply.github.com>
2020-10-29 20:18:24 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ CLS
 color 0A
 echo "wait opening web browser"
 CD %USERPROFILE%\selenium-temp\Scripts
-activate.bat & CD %USERPROFILE%\Desktop & python autoattendence.py 
+activate.bat & CD %USERPROFILE%\Desktop & python autoattendance.py 
 
 
  
