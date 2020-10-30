c8f6b0406a971fe0e1dec5404a19d30efd06f1bd
kamlesh singh <30825138+kamlesh78@users.noreply.github.com>
2020-10-30 15:57:40 +0530

    Update autoattendance.py

--- autoattendance.py
+++ autoattendance.py
@@ -56,7 +56,22 @@ def tmc103():
 		print("Attendance already marked")
 		return
 
-
+	
+def tmc104():
+	try:
+		driver.get("http://45.116.207.86/moodle/course/view.php?id=26")
+		d = driver.find_elements_by_class_name("togglecompletion")
+		for form in d:
+			b = form.find_element_by_tag_name('button')
+			b.send_keys(Keys.DOWN)
+			value = b.find_element_by_tag_name('img').get_attribute('src')
+			if value == "http://45.116.207.86/moodle/theme/image.php/boost/core/1602071174/i/completion-manual-n":
+				b.click()
+	except NoSuchElementException:
+		print("Attendance already marked")
+		return
+	
+	
 # TMC 106		
 def tmc106():
 	try:
@@ -77,9 +92,6 @@ def tmc106():
 tmc101()
 tmc102()
 tmc103()
+tmc104()
 tmc106()
-
-#TMC 104
-
-driver.get("http://45.116.207.86/moodle/course/view.php?id=26")	
-driver.execute_script("window.scrollTo(0,(document.body.scrollHeight/2))")
+ 
