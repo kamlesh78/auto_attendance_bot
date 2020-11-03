886f3a8de94b580f9811c39b2fe443c75079b07a
kamlesh singh <30825138+kamlesh78@users.noreply.github.com>
2020-11-03 12:05:19 +0530

    Update autoattendance.py
    multiple method for TMC104

--- autoattendance.py
+++ autoattendance.py
@@ -15,7 +15,6 @@ elem2 = driver.find_element_by_id("password").send_keys(password)
 driver.find_element_by_id("loginbtn").click()
 
 # TMC 101
-
 def tmc101():
 	try:
 		driver.get("http://45.116.207.86/moodle/mod/attendance/view.php?id=473")
@@ -49,6 +48,7 @@ def tmc103():
 		driver.get("http://45.116.207.86/moodle/mod/attendance/view.php?id=624")
 		d=driver.find_element_by_partial_link_text("Submit attendance")
 		d.send_keys(Keys.DOWN)
+		
 		d.click()
 		driver.find_element_by_id("id_status_317").click()
 		driver.find_element_by_id("id_submitbutton").click()
@@ -59,17 +59,63 @@ def tmc103():
 	
 def tmc104():
 	try:
-		driver.get("http://45.116.207.86/moodle/course/view.php?id=26")
-		d = driver.find_elements_by_class_name("togglecompletion")
-		for form in d:
-			b = form.find_element_by_tag_name('button')
-			b.send_keys(Keys.DOWN)
-			value = b.find_element_by_tag_name('img').get_attribute('src')
-			if value == "http://45.116.207.86/moodle/theme/image.php/boost/core/1602071174/i/completion-manual-n":
-				b.click()
+		driver.get("http://45.116.207.86/moodle/mod/attendance/view.php?id=1167")
+		d=driver.find_element_by_partial_link_text("Submit attendance")
+		d.send_keys(Keys.DOWN)
+		
+		d.click()
+		driver.find_element_by_id("id_status_821").click()
+		driver.find_element_by_id("id_submitbutton").click()
 	except NoSuchElementException:
 		print("Attendance already marked")
 		return
+		
+# TMC 104
+
+
+
+"""def tmc104_temp():
+
+		driver.get("http://45.116.207.86/moodle/course/view.php?id=26")
+		list = driver.find_elements_by_class_name("activity.attendance.modtype_attendance")
+		for l in list:
+			a = l.find_element_by_xpath(".//div/div/div[2]/div[1]/a")
+			link =  a.get_attribute("href")
+			button = l.find_element_by_xpath(".//div/div/div[2]/div[2]/form/div/button")
+			button.send_keys(Keys.DOWN)
+			
+			value = button.find_element_by_xpath(".//img").get_attribute("src")
+			
+			if value == "http://45.116.207.86/moodle/theme/image.php/boost/core/1602071174/i/completion-manual-n":
+				button.click()
+				
+				driver.execute_script("window.open('');")
+				time.sleep(2)
+				driver.switch_to.window(driver.window_handles[1])
+				
+				driver.get(link)
+				time.sleep(2)
+				try:
+					d = driver.find_element_by_partial_link_text("Submit attendance")
+					d.click()
+					driver.find_element_by_id("id_status_821").click()
+					driver.find_element_by_id("id_submitbutton").click()
+				except NoSuchElementException:
+					driver.close()
+					driver.switch_to.window(driver.window_handles[0])
+				
+"""			
+			
+	
+		
+
+
+
+
+
+
+
+
 	
 # TMC 105
 def tmc105():
@@ -107,3 +153,4 @@ tmc104()
 tmc105()
 tmc106()
  
+driver.close()
